ad2a648b68075b09d3b0e5063b4fc3d70a5fe718
Aaron Sikes <aaron@sikes.io>
2017-06-13 10:59:27 -0400

    Get it working, by copying in some @cardstack/models code
    For some reason, the code generation didn't seem to be working properly.

--- app/models/rental.js
+++ app/models/rental.js
@@ -1,6 +1,16 @@
-import Rental from '@cardstack/hub/models/rental';
 import Ember from 'ember';
-export default Rental.extend({
+import BaseModel from '@cardstack/models/model';
+import DS from 'ember-data';
+
+export default BaseModel.extend({
+  title: DS.attr({fieldType: '@cardstack/core-types::string'}),
+  owner: DS.attr({fieldType: '@cardstack/core-types::string'}),
+  city: DS.attr({fieldType: '@cardstack/core-types::string'}),
+  propertyType: DS.attr({fieldType: '@cardstack/core-types::string'}),
+  image: DS.attr({fieldType: '@cardstack/core-types::string'}),
+  description: DS.attr({fieldType: '@cardstack/core-types::string'}),
+  bedrooms: DS.attr({fieldType: '@cardstack/core-types::integer'}),
+
   sleeps: Ember.computed('bedrooms', function() {
     return this.get('bedrooms') * 4;
   })
